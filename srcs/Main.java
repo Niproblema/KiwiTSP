d4b2ab233a5d54573e839f0f2c9af48d2b1d8a40
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2018-10-20 19:55:41 +0200

    Parsing pt.3

--- srcs/Main.java
+++ srcs/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -7,17 +8,20 @@ public class Main{
 
     static int N;
     static Area arStart;
+
+    /* Collecton of all data, parsed in main. */
     static class Data {
         static HashMap<String, Area> areas;
         static HashMap<String, Airport> airports;
-        static HashMap<String, Flight> flights;
+        static HashMap<Integer, Flight> flights;
     }
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        String start = scanner.next();
-        scanner.nextLine();
+
+        String[] temp = scanner.nextLine().split(" ");
+        N = Integer.parseInt(temp[0]);
+        String start = temp[1];
 
         Data.areas = new HashMap<>(N);
         Data.airports = new HashMap<>();
@@ -29,14 +33,20 @@ public class Main{
             String[] airports = scanner.nextLine().split(" ");
             Area inArea = new Area(name, airports);
             Data.areas.put(inArea.name, inArea);
+            Data.airports.putAll(inArea.areaAirports);
         }
         arStart = Data.areas.get("start");
 
+        while(scanner.hasNextLine()){
+            String[] inFlightLine = scanner.nextLine().split(" ");
+            if(inFlightLine.length < 4)break;
+            Flight inFlight = new Flight(inFlightLine[0], inFlightLine[1], Integer.parseInt(inFlightLine[2]), Integer.parseInt(inFlightLine[3]));
+            Data.flights.put(inFlight.id, inFlight);
+        }
+        //Parse finish
 
 
-        //System.out.println(N+" "+start);
-
-
+        System.out.println("lala");
     }
 
 
@@ -48,20 +58,41 @@ public class Main{
 
 class Area{
     String name;
+    HashMap<String, Airport> areaAirports;
 
-    public Area(String name, String[] airports){
+    public Area(String name, String[] inAirports){
+        this.areaAirports = new HashMap<>(inAirports.length);
         this.name = name;
-
-        for(int i = 0; i < airports.length; i++){
-            System.out.print(airports[i]);
+        for(String inAirport:inAirports){
+            areaAirports.put(inAirport, new Airport(inAirport));
         }
     }
 }
 
 class Airport{
-
+    String name;
+    HashMap<Integer, Flight> flightsIn,flightsOut;
+    public Airport(String name){
+        this.name = name;
+        this.flightsIn = new HashMap<>();
+        this.flightsOut = new HashMap<>();
+    }
 }
 
 class Flight{
+    private static int idCounter = 0; //Unique id counter
+    int id;                           //Unique id
+    Airport airportDeparture, airportDestination;
+    int date, cost;
+
+    public Flight(String departure, String destination, int date, int cost){
+        this.id = idCounter++;
+        airportDeparture = Main.Data.airports.get(departure);
+        airportDestination = Main.Data.airports.get(destination);
+        this.date = date;
+        this.cost = cost;
+        airportDeparture.flightsOut.put(id, this);
+        airportDestination.flightsIn.put(id, this);
+    }
 
 }
